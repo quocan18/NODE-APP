cd7e6316f1a4edc103e348cf6c943f69f2e0aba1
user.name like: "quocan18 <user.email like: "quocan18>
2022-05-03 15:20:45 +0700

    Returning a 404 page

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const express = require("express"); // Import express framework
 const app = express();
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const adminRoutes = require("./routes/admin"); // Import router admin
 const shopRoutes = require("./routes/shop");
@@ -13,6 +14,6 @@ app.use("/admin", adminRoutes); // Phân luồn đường dẫn cho admin
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 app.listen(3000); // Server sẽ chạy trên cổng 3000
