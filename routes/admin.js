23a76b4ac1ebe254441dc154942abd6288ca21bb
user.name like: "quocan18 <user.email like: "quocan18>
2022-05-03 15:29:09 +0700

    Using Helper Function for Navigation

--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,13 @@ const path = require("path");
 
 const express = require("express");
 
+const rootDir = require("../helper/path");
+
 const router = express.Router();
 
 // /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "add_product.html"));
+  res.sendFile(path.join(rootDir, "views", "add_product.html"));
 });
 
 // /admin/add-product => POST
