fe006a806b72f9c26bfb400816e291df56f38b9f
user.name like: "quocan18 <user.email like: "quocan18>
2022-05-03 12:31:48 +0700

    Serving HTML Pages

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,12 @@
+const path = require("path");
+
 const express = require("express");
 
 const router = express.Router();
 
 // /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-  res.send("<h1>Hello</h1>");
+  res.sendFile(path.join(__dirname, "../", "views", "add_product.html"));
 });
 
 // /admin/add-product => POST
