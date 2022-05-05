501858b9f0e6e49f12c5cd63dffb483c09c88d9c
user.name like: "quocan18 <user.email like: "quocan18>
2022-05-05 19:20:04 +0700

    Finishing The Controllers

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 const bodyParser = require("body-parser");
 const path = require("path");
 
+const errorController = require("./controllers/error");
+
 app.engine(
   "hbs",
   expressHbs({
@@ -26,7 +28,5 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminRoutes); // Phân luồn đường dẫn cho admin
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found" }); // Import 404.pug
-});
+app.use(errorController.get404);
 app.listen(3000); // Server sẽ chạy trên cổng 3000
