c3e5668ff0cbf761773c6faf1b2d7cbfc7697365
user.name like: "quocan18 <user.email like: "ankyunguyen2000@gmail.com>
2022-05-11 14:07:42 +0700

    Creating & Managing a Dummy User

--- app.js
+++ app.js
@@ -14,19 +14,40 @@ const sequelize = require("./util/database");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const User = require("./models/user");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  User.findById(1)
+    .then((user) => {
+      req.user = user;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
 sequelize
+  // .sync({force: true})
   .sync()
   .then((result) => {
-    console.log(result);
+    return User.findById(1);
+    //console.log(result);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: "John", email: "test@test.com" });
+    }
+    return user;
+  })
+  .then((user) => {
+    //console.log(user);
     app.listen(3000);
   })
   .catch((err) => {
