5302acb4ef608d5889902426d59516ad05ee5dd5
user.name like: "quocan18 <user.email like: "ankyunguyen2000@gmail.com>
2022-05-13 09:36:46 +0700

    Installing the MongoDB Driver

--- app.js
+++ app.js
@@ -4,66 +4,30 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
-const Order = require("./models/order");
-const OrderItem = require("./models/order-item");
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-const sequelize = require("./util/database");
-
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const User = require("./models/user");
-const Cart = require("./models/cart");
-const CartItem = require("./models/cart-product");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  User.findById(1)
-    .then((user) => {
-      req.user = user;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  // User.findById(1)
+  //   .then((user) => {
+  //     req.user = user;
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
 });
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Order, { through: OrderItem });
-
-sequelize
-  //.sync({ force: true })
-  .sync()
-  .then((result) => {
-    return User.findById(1);
-    //console.log(result);
-  })
-  .then((user) => {
-    if (!user) {
-      return User.create({ name: "John", email: "test@test.com" });
-    }
-    return user;
-  })
-  .then((user) => {
-    return user.createCart();
-  })
-  .then((cart) => {
-    app.listen(3000);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+//app.listen(3000, () => console.log("server is running in port 3000"));
