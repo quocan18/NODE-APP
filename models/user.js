5116d0fa12f063a715c5fa61403a6f132f68b425
user.name like: "quocan18 <user.email like: "ankyunguyen2000@gmail.com>
2022-05-16 09:50:08 +0700

    Creating New Users

--- models/user.js
+++ models/user.js
@@ -1,16 +1,25 @@
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
+const getDb = require("../util/database").getDb;
 
-const sequelize = require("../util/database");
+const ObjectId = mongodb.ObjectId;
+class User {
+  constructor(username, email) {
+    this.username = username;
+    this.email = email;
+  }
 
-const User = sequelize.define("User", {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING,
-});
+  save() {
+    const db = getDb();
+    return db.collection("users").insertOne(this);
+  }
+
+  static findById(userId) {
+    const db = getDb();
+    return db
+      .collection("users")
+      .findOne({ _id: new ObjectId(userId) })
+      .next();
+  }
+}
 
 module.exports = User;
