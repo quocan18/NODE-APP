bd981b1a3591cc461a1e56c8f0e23a9ecd79a1b6
user.name like: "quocan18 <user.email like: "ankyunguyen2000@gmail.com>
2022-05-17 10:49:08 +0700

    Adding Relational Order Data

--- models/user.js
+++ models/user.js
@@ -78,11 +78,19 @@ class User {
 
   addOrder() {
     const db = getDb();
-    return db
-      .collection("orders")
-      .insertOne(this.cart)
+    return this.getCart()
+      .then((products) => {
+        const order = {
+          items: products,
+          user: {
+            _id: new ObjectId(this._id),
+            name: this.name,
+          },
+        };
+        return db.collection("orders").insertOne(order);
+      })
       .then((result) => {
-        this.cart = { tiems: [] };
+        this.cart = { items: [] };
         return db
           .collection("users")
           .updateOne(
